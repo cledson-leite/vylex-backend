ecb86e30e4c8ea8aa86e95aaf8bc1132dee83c50
cledson leite <40147313+cledson-leite@users.noreply.github.com>
2024-08-16 20:05:30 -0300

    feat: implement infrastructure for product list

--- src/infra/db_client/prisma/db_prisma.client.spec.ts
+++ src/infra/db_client/prisma/db_prisma.client.spec.ts
@@ -1,5 +1,5 @@
-import { ICreateProductClient } from '@/data/source/create_product_client.interface..source';
 import { PrismaService } from '@/presentation/prisma/prisma.service';
+import { PaginationDto, ResponseDto } from '@/shared/dto';
 import { faker } from '@faker-js/faker';
 import { Test, TestingModule } from '@nestjs/testing';
 import { DbPrismaClient } from '.';
@@ -11,7 +11,7 @@ describe('Db Prisma Client', () => {
     quantity: faker.number.int({ min: 0, max: 100 }),
   };
   let prisma: PrismaService;
-  let sut: ICreateProductClient;
+  let sut: DbPrismaClient;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,7 +22,8 @@ describe('Db Prisma Client', () => {
           useValue: {
             product: {
               create: jest.fn(),
-              save: jest.fn(),
+              findMany: jest.fn(),
+              count: jest.fn(),
             },
           },
         },
@@ -31,17 +32,74 @@ describe('Db Prisma Client', () => {
     prisma = module.get<PrismaService>('prisma');
     sut = module.get<DbPrismaClient>(DbPrismaClient);
   });
-  it('should call the create method with the correct data', async () => {
-    sut.save(dto);
-    expect(prisma.product.create).toHaveBeenCalledWith({
-      data: dto,
+  describe('Save', () => {
+    it('should call the create method with the correct data', async () => {
+      sut.save(dto);
+      expect(prisma.product.create).toHaveBeenCalledWith({
+        data: dto,
+      });
+    });
+
+    it('should throw error received from client', async () => {
+      jest
+        .spyOn(prisma.product, 'create')
+        .mockRejectedValueOnce(new Error('Error'));
+      await expect(sut.save(dto)).rejects.toThrow();
     });
   });
+  describe('FindAll', () => {
+    const dto: PaginationDto = {
+      page: faker.number.int({ min: 1, max: 10 }),
+      limit: faker.number.int({ min: 1, max: 10 }),
+    };
+    const total = faker.number.int({ min: 1, max: 100 });
+    const item = Array(dto.limit)
+      .fill(null)
+      .map(() => ({
+        name: faker.commerce.productName(),
+        price: Number(faker.commerce.price()),
+        quantity: faker.number.int({ min: 0, max: 100 }),
+      }));
+    const response: ResponseDto = {
+      item,
+      meta: {
+        totalItems: total,
+        itemCount: item.length,
+        itemsPerPage: dto.limit,
+        totalPages: Math.ceil(total / dto.limit),
+        currentPage: dto.page,
+      },
+    };
+    it('implement call the find all method without parameter', async () => {
+      sut.getAll();
+      expect(prisma.product.findMany).toHaveBeenCalledWith();
+    });
 
-  it('should throw error received from client', async () => {
-    jest
-      .spyOn(prisma.product, 'create')
-      .mockRejectedValueOnce(new Error('Error'));
-    await expect(sut.save(dto)).rejects.toThrow();
+    it('should throw error received from client', async () => {
+      jest
+        .spyOn(prisma.product, 'findMany')
+        .mockRejectedValueOnce(new Error('Error'));
+      await expect(sut.getAll()).rejects.toThrow();
+    });
+    it('should return an empty array', async () => {
+      jest.spyOn(prisma.product, 'findMany').mockResolvedValueOnce([]);
+      const result = await sut.getAll();
+      expect(result).toEqual({});
+    });
+    it('should return a list of products without pagination', async () => {
+      jest
+        .spyOn(prisma.product, 'findMany')
+        .mockResolvedValueOnce(response.item as any);
+      const result = await sut.getAll();
+      expect(result).toEqual({ item: [...response.item] });
+    });
+    it('should return a list of products with pagination', async () => {
+      jest
+        .spyOn(prisma.product, 'findMany')
+        .mockResolvedValueOnce(response.item as any);
+      jest.spyOn(prisma.product, 'count').mockResolvedValueOnce(total);
+      const result = await sut.getAll(dto);
+      expect(result).toEqual(response);
+    });
   });
 });
