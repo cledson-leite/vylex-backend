c55f2ee4430a4c4ddccbc1fddb824a0b4c8bf249
cledson leite <40147313+cledson-leite@users.noreply.github.com>
2024-08-18 18:07:53 -0300

    feat: implement control and service to update product

--- src/application/usecase/update/index.ts
+++ src/application/usecase/update/index.ts
@@ -1,7 +1,8 @@
+import { IUpdateProductUseCase } from '@/application/protocol/usecase/update_product.interface.usecase';
 import { UpdateDto } from '@/shared/dto';
 import { IUpdateProductRepository } from '../../protocol/reposirory/update_product.interface.repository';
 
-export class UpdateProductUseCase {
+export class UpdateProductUseCase implements IUpdateProductUseCase {
   constructor(private readonly repository: IUpdateProductRepository) {}
   async execute(dto: UpdateDto): Promise<void> {
     const { name, proprity, value } = dto;
